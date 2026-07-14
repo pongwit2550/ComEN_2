49d9405aa6f75ce17dd8e6a3522cb905ff606020
pongwit2550 <pongwitkuntong@gmail.com>
2026-07-14 11:51:37 +0700

    Recursive

--- resursive.c
+++ resursive.c
@@ -16,86 +16,84 @@
     }*/
 
 
-/*  ข้อ 2
+// ข้อ 2
 int NDown(int n){
     printf("Iterative");
     while (n >= 1) {
-       printf("%d  ", n--);
+       printf("%d  \n", n--);
     }
-    printf("Exit Number of n is 1!");
+    printf("Exit Number of n is 1! \n");
 }
 
 void NDownR(int n){
     if (n == 1) {
-        printf("N  : %d", n); 
+        printf("Recursive  : %d", n); 
     }
     else if (n <= 0) {
-        printf("is Lower that 1 : %d \n", n);
+        printf("Recursive is Lower that 1 : %d \n", n);
         return NDownR(n + 1);
     }else {
-        printf("N  : %d \n", n);
+        printf("Recursive  : %d \n", n);
         return NDownR(n - 1);
     }
  
 }
-*/
 
-/* ข้อ 3
+
+/*
+// ข้อ 3
 void printToN(int n){
     for(int i = 1 ; i <= n; i++){
-        printf("%d",i);
+        printf("Iterative : %d \n",i);
     }
 }
 
 void printToNR(int i , int n){
     if (i == n) {
-        printf("num :  %d \n", i);
+        printf("Recursive num :  %d \n", i);
     }
     else if (i >= n) {
-       
-        printf("num :  %d \n", i);
+        
+        printf("Recursive num :  %d \n", i);
         return printToNR(i - 1, n);
     }
     else {
-       
-        printf("num :  %d \n", i);
+        
+        printf("Recursive num :  %d \n", i);
         return  printToNR(i + 1, n); 
     } 
-
-}*/
-
+    
+}
+ */
+/*
 int sum ;
 int instance_num = 1;
 int sumToN_Iterative(int n){
-
-  for (int i = 1; i <= n; i++) {
-      sum += i;
-      printf("%d \n",i);
-  }
-  
-  return sum;
-
+    
+    for (int i = 1; i <= n; i++) {
+        sum += i;
+        printf("%d \n",i);
+    }
+    
+    return sum;
+    
 }
 
 int sumToN_Recursive(int i, int n) {
-    // Base case: stop when i reaches or passes n
+    
     if (i > n) { 
         return 0; 
     }
-    
-    // Print current step
+
     printf("%d\n", i);
-    
-    // Recursive step: Add current 'i' to the sum of the rest of the numbers
     return i + sumToN_Recursive(i + 1, n);
 }
 
+*/
 
 
 int main (){
-
-    printf("Sum of Iterativer: %d \n", sumToN_Iterative(5));
-
-    printf("Sum of Recursive:  %d \n", sumToN_Recursive(1, 5));
+    NDown(6);
+    NDownR(5);
 
 }
