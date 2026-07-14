c2585d440206b09221eb70dec1ebe8e2f861b731
pongwit2550 <pongwitkuntong@gmail.com>
2026-07-14 09:53:50 +0700

    Recursive

---
+++ resursive.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+
+
+    /*void printDown(int n){
+        if (n <= 1 ){
+            printf("%d ",n);   
+            printDown(n-1);  
+        }
+
+        if (n >= 1) {
+           
+            printf("%d ",n);   
+            printDown(n-1);
+        }
+    }*/
+/*  ข้อ 2
+int NDown(int n){
+    printf("Iterative");
+    while (n >= 1) {
+       printf("%d  ", n--);
+    }
+    printf("Exit Number of n is 1!");
+}
+
+void NDownR(int n){
+    if (n == 1) {
+        printf("N  : %d", n); 
+    }
+    else if (n <= 0) {
+        printf("is Lower that 1 : %d \n", n);
+        return NDownR(n + 1);
+    }else {
+        printf("N  : %d \n", n);
+        return NDownR(n - 1);
+    }
+ 
+}
+*/
+int main (){
+    // ข้อ 2
+    /*
+    NDown(5);
+    NDownR(6);
+    */
+}
